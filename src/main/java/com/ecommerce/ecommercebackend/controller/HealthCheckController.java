df933a97dd0d13c88588d4b7e07aa88359ccdeb4
kenwong <20069234+kenwong9508@users.noreply.github.com>
2026-06-13 15:27:45 +0800

    chore: initial setup

--- src/main/java/com/ecommerce/ecommercebackend/controller/HealthCheckController.java
+++ src/main/java/com/ecommerce/ecommercebackend/controller/HealthCheckController.java
@@ -1,4 +1,18 @@
 package com.ecommerce.ecommercebackend.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthCheckController {
+
+  @GetMapping("/health-check")
+  public Map<String, String> healthCheck() {
+    Map<String, String> response = new HashMap<>();
+    response.put("status", "OK");
+    response.put("message", "Application is running smoothly");
+    return response;
+  }
 }
