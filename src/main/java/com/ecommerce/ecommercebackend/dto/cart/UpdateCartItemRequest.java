adb7c6ce89c88d82ddeed34dcf559c91b38135b0
kenwong <20069234+kenwong9508@users.noreply.github.com>
2026-06-26 15:11:29 +0800

    chore: add @Builder, @AllArgsConstructor, @NoArgsConstructor in dto "UpdateCartItemRequest"

--- src/main/java/com/ecommerce/ecommercebackend/dto/cart/UpdateCartItemRequest.java
+++ src/main/java/com/ecommerce/ecommercebackend/dto/cart/UpdateCartItemRequest.java
@@ -2,9 +2,15 @@ package com.ecommerce.ecommercebackend.dto.cart;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateCartItemRequest {
 
     @NotNull(message = "Quantity cannot be null")
